80cf637c992a84af092a065ab357f1e8f47f3355
Carlos Perales <carlosperales050@gmail.com>
2024-06-13 18:52:25 -0600

    add: /api-docs

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
 const { config } = require("../config/config");
 const app = express();
 const userRouter = require("./components/user/route");
+
+const swaggerDoc = require("./swagger.json");
+
 app.use(express.json());
 // routes
 app.use("/api/user", userRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.port, () => {
   console.log(`listening at http://localhost:${config.port}`);
