f67e46f359382adf5da25bf64a8eccf5c8f692cb
Carlos Perales <carlosperales050@gmail.com>
2024-06-26 18:02:10 -0600

    agregamos variables

--- config/config.js
+++ config/config.js
@@ -3,6 +3,15 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET,
+  mysqlHost: process.env.DB_HOST,
+  mysqlUser: process.env.DB_USER,
+  mysqlPassword: process.env.DB_PASSWORD,
+  mysqlDatabase: process.env.DB_NAME,
+  mysqlPort: process.env.DB_PORT,
+  pgAdminMail: process.env.PG_MAIL,
+  pgAdminPassword: process.env.PG_PASSWORD,
+  pgAdminPort: process.env.PG_PORT,
+  pgAdminRefPort: process.env.PG_REFPORT,
 };
 
 module.exports = { config };
