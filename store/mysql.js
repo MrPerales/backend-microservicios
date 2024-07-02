f9fa54d22126ddc6af04640efba63579ea20782b
Carlos Perales <carlosperales050@gmail.com>
2024-07-02 15:58:19 -0600

    completando la base de datos

--- store/mysql.js
+++ store/mysql.js
@@ -44,6 +44,67 @@ function list(table) {
   });
 }
 
+function get(table, id) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table} WHERE id=${id}`, (err, data) => {
+      console.log(data);
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+}
+
+function insert(table, data) {
+  return new Promise((resolve, reject) => {
+    connection.query(`INSERT INTO ${table} SET ?`, data, (err, resp) => {
+      if (err) return reject(err);
+      resolve(resp);
+    });
+  });
+}
+function update(table, data) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `UPDATE ${table} SET ? WHERE id=? `,
+      [data, data.id],
+      (err, resp) => {
+        if (err) return reject(err);
+        resolve(resp);
+      }
+    );
+  });
+}
+function upsert(table, data) {
+  return new Promise((resolve, reject) => {
+    // ON DUPLICATE KEY UPDATE: Indica que si se encuentra una fila
+    // con una clave duplicada (en este caso, id),
+    // se deben actualizar las columnas especificadas.
+    // asi ya no ocupamos de las otras fucniones insert y update
+    // pero recuerda poner el id unico en mysql
+    connection.query(
+      `INSERT INTO ${table} SET ? ON DUPLICATE KEY UPDATE ?`,
+      [data, data],
+      (err, resp) => {
+        console.log("UPDATE DATA:", resp);
+        if (err) return reject(err);
+        const user = resp[0] || null;
+        resolve(user);
+      }
+    );
+  });
+}
+
+function query(table, query) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table} WHERE ?`, query, (err, resp) => {
+      if (err) return reject(err);
+      resolve(resp[0]);
+    });
+  });
+}
 module.exports = {
   list,
+  get,
+  upsert,
+  query,
 };
